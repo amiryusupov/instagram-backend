3e82f0546bd8291911ee33077545f88e381eb04c
Amir Yusupov <amiryusupov.070@gmail.com>
2024-08-08 18:32:46 +0500

    feat: make error-handler general

--- src/api/v1/auth.ts
+++ src/api/v1/auth.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import register from "../../controllers/auth/register.controller"
-import errorHandler from "../../middleware/errorHandler";
 import verification from "../../controllers/auth/verification.controller"
 
 const router = Router()
 
-.post("/register", register, errorHandler)
+.post("/register", register)
 .post("/verify", verification)
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import api from "./api/routes";
 import { serverConfig } from "./config/index";
+import errorHandler from "./middleware/errorHandler";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", api);
+app.use(errorHandler);
 
 app.listen(serverConfig.port, () => {
   console.log(`Server is running on http://localhost:${serverConfig.port}`);
